a5bbb9389b5b8882678da2622f5481e7d37e0a8d
sofianh <sofian.hadi1983@gmail.com>
2025-03-31 12:56:48 +0700

    fix grammar dasar

--- parsing.c
+++ parsing.c
@@ -18,22 +18,30 @@ int main(int argc, char** argv) {
     mpc_parser_t* Lispy     = mpc_new("lispy");
 
     mpca_lang(MPCA_LANG_DEFAULT,
-        "\
-        number      : /-?[0-9]+/ ;                                  \
-        operator    : '+' | '-' | '*' | '/' | '//' | '%' ;          \
-        expr        : <number>  | '(' <operator> <expr>+ ')' ;      \
-        lispy       :  /^/ <operator> <expr>+ /$/ ;                 \
+        "                                                     \
+           number   : /-?[0-9]+/ ;                            \
+           operator : '+' | '-' | '*' | '/' | \"//\" | '%';   \
+           expr     : <number> | '(' <operator> <expr>+ ')' ; \
+           lispy    :  /^/ <operator> <expr>+ /$/ ;           \
         ", 
         Number, Operator, Expr, Lispy);
-
-
+        
     puts("Lispy versi 0.0.1");
     puts("Tekan CTRL+C untuk keluar\n");
     
     while(1) {
         char* input = readline(">>> ");
         add_history(input);
-        printf("Anda memasukan perintah %s\n", input);
+        
+        mpc_result_t r;
+        if (mpc_parse("<stdin>", input, Lispy, &r)) {
+            mpc_ast_print(r.output);
+            mpc_ast_delete(r.output);
+        } else {
+            mpc_err_print(r.error);
+            mpc_err_delete(r.error);
+        }
+
         free(input);
     }
 
